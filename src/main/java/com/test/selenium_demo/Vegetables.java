b1c5720e5c2e9cd7081d4eb39a337e435ffa5630
nssnprasad-alt <nssnprasad@gmail.com>
2026-04-26 18:23:25 +0530

    Jai Hind

--- src/main/java/com/test/selenium_demo/Vegetables.java
+++ src/main/java/com/test/selenium_demo/Vegetables.java
@@ -18,7 +18,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Vegetables {
-     @Test
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
